40b55cf914d63b46fa0a8e5de084b55ca973c58d
rajanirajen <88222668+rajanirajen@users.noreply.github.com>
2025-04-04 08:09:01 +0530

    Update aws.infra.tf

--- aws.infra.tf
+++ aws.infra.tf
@@ -1,1 +1,26 @@
+provider "aws" {
+region = "us-east-1"
 
+}
+
+data "aws_ami" "myami"{
+
+owners = ["amazon"]
+most_recent = true
+
+filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+
+}
+
+resource "aws_instance" "myec2" {
+  ami    = data.aws_ami.myami.id 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-1"
+  }
+}
